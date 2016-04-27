d75e9f4844c54ab79d3191a5b20ce5aa58ae3465
colvardrm <rmc56824@uga.edu>
2016-04-27 13:27:12 -0400

    Added files via upload

---
+++ NewAssets/Scripts/Player.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+    public UnityStandardAssets.Characters.FirstPerson.FirstPersonController fps;
+    public GameObject cameraControls;
+    public bool isSpeaker = false;
+    PhotonVoiceRecorder rec;
+    public GameObject instructionsText;
+    public int countdownSeconds = 3;
+    public int currentSeconds = 0;
+    public TextMesh countdownText;
+    public TextMesh timerText;
+    bool isRunning = false;
+    //VoiceChatNetworkProxy proxy;
+    /*
+    void OnConnectedToServer()
+    {
+        proxy = VoiceChatUtils.CreateProxy();
+    }
+    */    
+
+    // Use this for initialization
+    void Start () {
+        if (!isLocalPlayer)
+        {
+            fps.enabled = false;
+            cameraControls.SetActive(false);
+        }
+        if (isSpeaker)
+        {
+            //fps.enabled = false;
+            rec = gameObject.GetComponent<PhotonVoiceRecorder>();
+            rec.Transmit = true;
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        IEnumerator timer;
+        IEnumerator countdown;
+        if (rec != null && Input.GetKeyDown(KeyCode.S))
+        {
+            instructionsText.SetActive(false);
+            if (!isRunning) {
+                isRunning = true;
+                //countdown = StartCoroutine(countdown());
+                rec.Transmit = true;
+                //timer = StartCoroutine(timer());
+            }
+            else
+            {
+                isRunning = false;
+                //StopCoroutine(countdown);
+                //StopCoroutine(timer);
+            }
+        }
+       
+
+    }
+
+    IEnumerator countdown()
+    {
+        countdownText.gameObject.SetActive(true);
+        countdownText.text = countdownSeconds.ToString();
+        while (countdownSeconds > 0)
+        {
+            yield return new WaitForSeconds(1.0f);
+            countdownSeconds--;
+            countdownText.text = countdownSeconds.ToString();
+        }
+        countdownText.gameObject.SetActive(false);
+    }
+
+    IEnumerator timer()
+    {
+        timerText.gameObject.SetActive(true);
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            currentSeconds++;
+            if(currentSeconds%60 > 10) {
+                timerText.text = (currentSeconds / 60).ToString() + ":" + (currentSeconds % 60).ToString();
+            }
+            else
+            {
+                timerText.text = (currentSeconds / 60).ToString() + ":0" + (currentSeconds % 60).ToString();
+            }
+
+        }
+    }
+
+
+}
---
+++ NewAssets/Scripts/SpeakerOrSpectator.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+using UnityEngine.Networking.NetworkSystem;
+
+public class SpeakerOrSpectator : NetworkManager {
+
+    public static bool speakerSpawned = false;
+    public GameObject playerPrefabSpecial;
+    public GameObject spectatorPrefab;
+    public GameObject mainCamera;
+    public GameObject speakerSpawn;
+    public Transform[] spectatorSpawn;
+    public NetworkManagerHUD managerHub;
+    byte numMatches = 1;
+    Dictionary<string, byte> matches = new Dictionary<string, byte>();
+
+
+
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        //mainCamera.SetActive(false);
+        GameObject player;
+        
+            if (!speakerSpawned)
+            {
+                player = (GameObject)GameObject.Instantiate(playerPrefabSpecial, speakerSpawn.transform.position, speakerSpawn.transform.rotation);
+                player.GetComponent<Player>().isSpeaker = true;
+                //Debug.Log("Spawning speaker");
+                speakerSpawned = true;
+                PhotonVoiceRecorder rec = player.GetComponent<PhotonVoiceRecorder>();
+                rec.Transmit = true;
+
+                PhotonVoiceNetwork.Client.DebugEchoMode = true;
+             }
+            else
+            {
+                int i = 0;
+                player = (GameObject)GameObject.Instantiate(spectatorPrefab, spectatorSpawn[i].position, spectatorSpawn[i].transform.rotation);
+            //Debug.Log("Speaker already spawned");
+            //PhotonVoiceRecorder.Transmit = false;
+            }
+         NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+
+
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        base.OnClientDisconnect(conn);
+
+    }
+    /*
+    public override void OnMatchCreate(CreateMatchResponse matchInfo)
+    {
+        if (matchInfo.success)
+        {
+            matches[NetworkManager.matchName] = numMatches;
+            PhotonVoiceNetwork.GlobalAudioGroup = matches[NetworkManager.matchName];
+            numMatches++;
+            StartHost(new MatchInfo(matchInfo));
+        }
+    }
+    */
+    // Use this for initialization
+    void Start() {
+        #if UNITY_5_3
+                                PhotonNetwork.ConnectUsingSettings(string.Format("1.{0}", UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex));
+        #else
+                PhotonNetwork.ConnectUsingSettings(string.Format("1.{0}", Application.loadedLevel));
+        #endif
+
+        PhotonVoiceNetwork.Connect();
+
+
+        PhotonVoiceNetwork.Client.GlobalAudioGroup = 0;
+        PhotonVoiceNetwork.Client.DebugEchoMode = true;
+
+    }
+
+    public void OnDisconnectedFromServer(NetworkDisconnection info)
+    {
+        PhotonNetwork.Disconnect();
+        PhotonVoiceNetwork.Disconnect();
+    }
+
+    public void OnJoinedRoom()
+    {
+        Debug.Log("joined room");
+        managerHub.enabled = true;
+    }
+
+        // Update is called once per frame
+    void Update () {
+
+    }
+}
